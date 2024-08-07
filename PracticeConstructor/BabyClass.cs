2afef97311494b77e08cdc6ceeb1e2557a4d1e83
canbe <canbe@CANBERK>
2024-08-08 00:18:35 +0300

    Add project files.

---
+++ PracticeConstructor/BabyClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeConstructor
+{
+    public class BabyClass
+    {
+        public string _name;
+
+        public string _surname;
+
+        public DateTime _birthDate;
+
+
+
+
+
+        public BabyClass()  //  without parameters default constructor
+        {
+            CryingBaby(); 
+        
+        }
+
+
+        public BabyClass(string name, string surname)  // Constructor with two parameters
+
+        {
+            _name = name;
+            _surname = surname;
+
+        
+        }
+
+
+        public void CryingBaby()    // crying message
+
+        {
+            Console.WriteLine("INGAAAAAAAAA");
+
+            _birthDate = DateTime.Now;
+        }
+
+        public void BabyDisplay() // display baby informations
+
+        {
+            Console.WriteLine($" name {_name}, surname {_surname}, birthday {_birthDate}");
+
+
+        }
+
+
+    }
+}
---
+++ PracticeConstructor/Program.cs
@@ -0,0 +1,20 @@
+﻿using PracticeConstructor; 
+{
+
+    BabyClass baby1 = new BabyClass(); // new object default constructors
+    baby1._name = "ramazan";
+    baby1._surname = "yıldırım";
+
+
+    baby1.BabyDisplay();
+
+
+    BabyClass baby2 = new BabyClass("ramazan", "yıldırım"); // two parameters constructor
+
+
+    baby2.BabyDisplay();
+
+
+
+
+}
